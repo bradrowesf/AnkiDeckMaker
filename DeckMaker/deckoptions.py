6360d1c535c9fa33557b0463a6aba78c6e4448ec
Bradley Rowe <bradrowesf@yahoo.com>
2016-06-26 16:06:11 -0700

    Factor argument adding. Test setup.

--- DeckMaker/deckoptions.py
+++ DeckMaker/deckoptions.py
@@ -4,8 +4,13 @@ class DeckOptions(object):
 	
 	def __init__(self, parser):
 		
-		parser.add_argument("pathToConfig")
+		self.addArgs(parser)
 		self.args = parser.parse_args()
+		
+	def addArgs(self, parser):
+	
+		parser.add_argument("pathToConfig", type=str,
+							help="Path to config file")
 		
 	def pathToConfig(self):
 	
---
+++ tests/deckoptions_tests.py
@@ -0,0 +1,11 @@
+from nose.tools import *
+from DeckMaker import deckoptions
+
+def setup():
+    print "SETUP!"
+
+def teardown():
+    print "TEAR DOWN!"
+
+def test_basic():
+    # Figure out someway to test.
