138bca2810af6dc3c4afcc0f7172164d0dba4da8
Bradley Rowe <bradrowesf@yahoo.com>
2016-07-01 22:53:55 -0700

    Add tests for configuration class

--- tests/deckconfig_tests.py
+++ tests/deckconfig_tests.py
@@ -1,2 +1,23 @@
 from nose.tools import *
-from DeckMaker import deckconfig
+from DeckMaker.deckconfig import Configuration
+
+class TestConfiguration(object):
+    @classmethod
+    def setup_class(self):
+       self.config = Configuration("bin/defaultconfig.json")
+
+    @classmethod
+    def teardown_class(self):
+        """This method is run once for each class _after_ all tests are run"""
+
+    def setUp(self):
+        """This method is run once before _each_ test method is executed"""
+
+    def teardown(self):
+        """This method is run once after _each_ test method is executed"""
+
+    def test_UpperRange(self):
+        assert_equal(self.config.getUpperRange(), "E4")
+        
+    def test_LowerRange(self):
+		assert_equal(self.config.getLowerRange(), "E2")
