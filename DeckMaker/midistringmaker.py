23fecea7f923abe640908223ba358b492ad458c9
Bradley Rowe <bradrowesf@yahoo.com>
2016-07-04 22:28:36 -0700

    refine the midistringmaker

--- DeckMaker/midistringmaker.py
+++ DeckMaker/midistringmaker.py
@@ -5,30 +5,48 @@ class MidiStringMaker(object):
 	def __init__(self, translator):
 	
 		self.myTranslator = translator
+		self.myKeyDown = str(90)
+		self.myKeyUp = str(80)
+		self.myVelocity = str(60)
+		self.ticksPerBeat = 960
 		
 	def AppendNote(self, note, placement, duration):
+		
+		keyDown =  self.KeyDown(note, placement, duration, True)
+		keyUp = self.KeyUp(note, placement, duration, False)
+		outString = keyDown + keyUp
+		
+		return outString
+		
+	def KeyDown(self, note, placement, duration, newline):
+		# note - pitch to add
+		# placement - beats following last event
+		# duration - beats note held
+
+		outString = "E "	# starts w/ this for some reason
+		outString += str(self.ticksPerBeat*placement) + " "	# 960 per beat
+		outString += self.myKeyDown + " "	# note down
+		outString += self.myTranslator.GetMidiNoteHexString(note) + " "
+		outString += self.myVelocity # velocity
+		if newline==True:
+			outString += '\n' 
+		
+		return outString
+		
+	def KeyUp(self, note, placement, duration, newline):
 		# note - pitch to add
 		# placement - beats following last event
 		# duration - beats note held
 		
-		#Build keydown
 		outString = "E "	# starts w/ this for some reason
-		outString += str(960*placement) + " "	# 960 per beat
-		outString += str(90) + " "	# note down
+		outString += str(self.ticksPerBeat*duration) + " "	# 960 per beat
+		outString += self.myKeyUp + " "	# note down
 		outString += self.myTranslator.GetMidiNoteHexString(note) + " "
-		outString += str(60) + '\n' # velocity
-		
-		#Build keyup
-		outString += "E "	# starts w/ this for some reason
-		outString += str(960*duration) + " "	# 960 per beat
-		outString += str(80) + " "	# note down
-		outString += self.myTranslator.GetMidiNoteHexString(note) + " "
-		outString += str(60) # velocity
+		outString += self.myVelocity + '\n' # velocity
+		if newline==True:
+			outString += '\n'
 		
 		return outString
-		
-		
-		
 		
 		
 if __name__ == "__main__":
